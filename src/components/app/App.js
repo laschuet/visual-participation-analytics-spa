1f2dc3dfd2f0773b999e07d2d93c465d97d9c3e6
laschuet <laschuet@users.noreply.github.com>
2020-10-13 09:01:05 +0200

    Move router

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,6 @@
 import { SquirrelIcon } from "@primer/octicons-react";
 import React from "react";
-import {
-  BrowserRouter as Router,
-  NavLink,
-  Route,
-  Switch,
-} from "react-router-dom";
+import { NavLink, Route, Switch } from "react-router-dom";
 
 import Analytics from "../Analytics";
 import Home from "../Home";
@@ -15,41 +10,39 @@ import "./App.scss";
 
 const App = () => {
   return (
-    <Router>
-      <div className="App">
-        <header className="Header px-4">
-          <div className="Header-item">
-            <NavLink to="/" className="Header-link">
-              <SquirrelIcon className="mr-2" />
-              ViPA
-            </NavLink>
-          </div>
-          <div className="Header-item">
-            <NavLink to="/analytics" className="Header-link">
-              Analytics
-            </NavLink>
-          </div>
-          <div className="Header-item">
-            <NavLink to="/settings" className="Header-link">
-              Settings
-            </NavLink>
-          </div>
-        </header>
-        <div className="p-4">
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/analytics">
-              <Analytics />
-            </Route>
-            <Route path="/settings">
-              <Settings />
-            </Route>
-          </Switch>
+    <div className="App">
+      <header className="Header px-4">
+        <div className="Header-item">
+          <NavLink to="/" className="Header-link">
+            <SquirrelIcon className="mr-2" />
+            ViPA
+          </NavLink>
         </div>
+        <div className="Header-item">
+          <NavLink to="/analytics" className="Header-link">
+            Analytics
+          </NavLink>
+        </div>
+        <div className="Header-item">
+          <NavLink to="/settings" className="Header-link">
+            Settings
+          </NavLink>
+        </div>
+      </header>
+      <div className="p-4">
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/analytics">
+            <Analytics />
+          </Route>
+          <Route path="/settings">
+            <Settings />
+          </Route>
+        </Switch>
       </div>
-    </Router>
+    </div>
   );
 };
 
