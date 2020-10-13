bda67e09bbf8d428a8089f31082ffe26a9ec0341
laschuet <laschuet@users.noreply.github.com>
2020-10-13 20:58:56 +0200

    Add icon to error message

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,8 +1,10 @@
+import { ShieldXIcon } from "@primer/octicons-react";
 import PropTypes from "prop-types";
 import React from "react";
 
 const Error = ({ message = "" }) => (
   <div className="flash flash-error">
+    <ShieldXIcon />
     {message === "" ? "Oops! An error occured..." : message}
   </div>
 );
