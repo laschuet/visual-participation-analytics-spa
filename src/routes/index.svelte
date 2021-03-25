41b1c62294b28cccde6e8b5abc94e99c36eee6f2
laschuet <laschuet@users.noreply.github.com>
2021-03-25 17:35:31 +0100

    Move contributions to central store

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,10 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 
-	import type { ContributionType } from '../types/contribution.type';
-
 	import Contribution from '$lib/Contribution.svelte';
-	import { incomingWebSocketMessage } from '../stores';
+	import { contributions, incomingWebSocketMessage } from '../stores';
 	import { createWebSocket } from '../webSocket';
 
-	let contributions: ContributionType[] = [];
 	let ws: WebSocket;
 
 	async function fetchContributions() {
@@ -15,7 +12,7 @@
 			'http://127.0.0.1:3010/api/v1/datasets/liqd_mauerpark.sqlite/contributions';
 		const res: Response = await fetch(url);
 		if (res.ok) {
-			contributions = await res.json();
+			$contributions = await res.json();
 		} else {
 			throw new Error(`Could not load ${url}`);
 		}
@@ -36,7 +33,7 @@
 	}}>click</button
 >
 
-{#each contributions as contribution}
+{#each $contributions as contribution}
 	<Contribution {contribution} />
 {:else}
 	Loading...
--- src/stores.ts
+++ src/stores.ts
@@ -1,4 +1,7 @@
 import { writable } from 'svelte/store';
 
+import type { ContributionType } from './types/contribution.type';
+
 export const alertMessage = writable<string>('');
-export const incomingWebSocketMessage = writable<string>('', null);
+export const incomingWebSocketMessage = writable<string>('');
+export const contributions = writable<ContributionType[]>([]);
